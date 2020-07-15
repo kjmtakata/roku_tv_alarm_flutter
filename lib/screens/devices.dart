189c6b580f9693fa858376eab88aa02c7d2f88a3
Kevin Takata <ktakata@novacoast.com>
2020-07-15 13:34:53 -0700

    Use stream for device discovery

--- lib/screens/devices.dart
+++ lib/screens/devices.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
 import "package:upnp/upnp.dart";
 
 class DevicesPage extends StatefulWidget {
@@ -8,45 +7,44 @@ class DevicesPage extends StatefulWidget {
 }
 
 class _DevicesPageState extends State<DevicesPage> {
-  List<Device> discoveredDevices = [];
-
-  RefreshController _refreshController =
-    RefreshController(initialRefresh: true);
-
-  void _onRefresh() async {
-    var discoverer = new DeviceDiscoverer();
-    await discoverer.start(ipv6: false);
-    this.discoveredDevices.clear();
-    for(DiscoveredClient discoveredClient in await discoverer.discoverClients()) {
-      this.discoveredDevices.add(await discoveredClient.getDevice());
-    }
-    _refreshController.refreshCompleted();
-    setState(() {});
-  }
+  Map<String,Device> discoveredDevices = {};
 
   @override
   Widget build(BuildContext context) {
+    var discoverer = new DeviceDiscoverer();
+    discoverer.start(ipv6: false).then((value) {
+      discoverer.quickDiscoverClients().listen((event) {
+        event.getDevice().then((device) {
+          if (mounted && !discoveredDevices.containsKey(device.uuid)) {
+            setState(() {
+              discoveredDevices.putIfAbsent(device.uuid, () => device);
+            });
+          }
+        });
+      });
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Devices'),
       ),
-      body: SmartRefresher(
-        enablePullDown: true,
-        controller: _refreshController,
-        onRefresh: _onRefresh,
-        child: ListView.builder(
-          itemBuilder: (context, i) {
-            if (i < this.discoveredDevices.length) {
-              return ListTile(
-                title: Text(this.discoveredDevices[i].friendlyName),
-                onTap: () => Navigator.pop(context),
-              );
-            }
-            else {
-              return null;
-            }
-          },
-        )
+      body: ListView.builder(
+        itemBuilder: (context, i) {
+          if (i < this.discoveredDevices.length) {
+            List<String> keys = discoveredDevices.keys.toList();
+            String title = discoveredDevices[keys[i]].friendlyName;
+            return ListTile(
+              title: Text(title),
+              onTap: () {
+                discoverer.stop();
+                Navigator.pop(context);
+              },
+            );
+          }
+          else {
+            return null;
+          }
+        },
       )
     );
   }
