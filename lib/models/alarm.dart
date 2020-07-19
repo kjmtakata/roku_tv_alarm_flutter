039883aef34e3fad6e329bdb5220eb77b955ca2f
Kevin Takata <ktakata@novacoast.com>
2020-07-18 20:00:45 -0700

    add day of week saving

--- lib/models/alarm.dart
+++ lib/models/alarm.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Alarm {
-  final int id;
-  final TimeOfDay time;
-  final String deviceUuid;
-  final String deviceName;
-  final String deviceUrl;
-  final String channel;
+  int id;
+  TimeOfDay time;
+  String deviceUuid;
+  String deviceName;
+  String deviceUrl;
+  String channel;
+  int dayMap;
 
-  Alarm(this.id, this.time, this.deviceUuid, this.deviceName,
-      this.deviceUrl, this.channel);
+  Alarm(int id, TimeOfDay time, String deviceUuid, String deviceName,
+      String deviceUrl, String channel, Map<int,bool> dayMap) {
+    this.id = id;
+    this.time = time;
+    this.deviceUuid = deviceUuid;
+    this.deviceName = deviceName;
+    this.deviceUrl = deviceUrl;
+    this.channel = channel;
+
+    this.dayMap = 0;
+    dayMap.forEach((key, value) {
+      this.dayMap |= ((value ? 1 : 0) << key);
+    });
+  }
 
   Alarm.fromJson(Map<String, dynamic> json)
       : id = json['id'],
@@ -17,7 +30,8 @@ class Alarm {
         deviceUuid = json['device_uuid'],
         deviceName = json['device_name'],
         deviceUrl = json['device_url'],
-        channel = json['channel'];
+        channel = json['channel'],
+        dayMap = json['day_map'];
 
   Map<String, dynamic> toJson() => {
     'id': id,
@@ -27,5 +41,8 @@ class Alarm {
     'device_name': deviceName,
     'device_url': deviceUrl,
     'channel': channel,
+    'day_map': dayMap,
   };
+
+  bool isDay(int day) => ((dayMap >> day) & 1) == 1;
 }
